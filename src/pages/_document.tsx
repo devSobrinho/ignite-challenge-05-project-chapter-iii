ab7c5462c4f3e78fc80fe820363e24d24b1d95aa
Daniel Sobrinho <dev.sobrinho@gmail.com>
2022-03-24 15:42:56 -0300

    Modifield file pages/_document.tsx, add font and faviicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,22 @@
-import Document from 'next/document';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
-  // render() {
-  //   //TODO
-  // }
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
+            rel="stylesheet"
+          />
+          <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+        </Head>
+        <Main />
+        <NextScript />
+      </Html>
+    );
+  }
 }
